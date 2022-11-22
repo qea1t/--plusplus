c67f4572664481df6ada88ea1d6a1d6eed193974
qea1t <91588459+qea1t@users.noreply.github.com>
2022-11-22 10:06:38 +0300

    Create task6.cpp

---
+++ week4/task6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <Math.h>
+#include <iomanip> 
+using namespace std;
+
+int main() {
+    bool answer;
+    int value;
+g:cout << "Enter a value: ";
+    cin >> value;
+    answer = ((value >= 0 && value < 10) || (value * 2 < 20 && value - 2 > -2) || (value - 1 > 1 && value / 2 < 10) || value == 111);
+    cout << (answer ? "THAT'S TRUE :)" : "THAT'S NOT TRUE :(") << endl;
+    goto g;
+}
